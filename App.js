bad83407678bf9f9343fbaa311c19dcc0cafc0c7
sakurai <39reyskhky@gmail.com>
2020-05-08 23:23:44 +0900

    Component作成

--- App.js
+++ App.js
@@ -1,56 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from './components/ListItem';
+import articles from './dummies/articles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row'
-  },
-  leftContainer: {
-    width: 100
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    padding: 10,
-    justifyContent: 'space-between'
-  },
-  text: {
-    fontSize: 16
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray'
+    backgroundColor: '#fff'
   }
 });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://picsum.photos/id/10/200/200' }} />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }

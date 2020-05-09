9251185d320e17c8687343bba9b157d63fc07bad
sakurai <39reyskhky@gmail.com>
2020-05-09 21:54:02 +0900

    WebView実装

--- App.js
+++ App.js
@@ -1,41 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import ListItem from './components/ListItem';
-import dummyArticles from './dummies/articles';
-import Constans from 'expo-constants';
-import axios from 'axios';
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constans.manifest.extra.newsApiKey}`;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  }
-});
-
-export default function App() {
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
-  );
-}
+export default App = () => {
+  return <AppNavigator />;
+};
